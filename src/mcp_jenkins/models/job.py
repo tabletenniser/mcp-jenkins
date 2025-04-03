49ad27bae084e6836f5bb12d39525754cdd49319
lanbaoshen <lanbaoshen@icloud.com>
2025-04-03 17:12:44 +0800

    Optimize BaseModel model config

--- src/mcp_jenkins/models/job.py
+++ src/mcp_jenkins/models/job.py
@@ -1,16 +1,17 @@
 from typing import Union
 
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, ConfigDict
 
 
 class JobBase(BaseModel):
+    model_config = ConfigDict(
+        validate_by_name=True
+    )
+
     class_: str = Field(..., alias='_class')
     name: str
     url: str
     fullname: str
-
-    class Config:
-        validate_by_name = True
 
 
 class Job(JobBase):
