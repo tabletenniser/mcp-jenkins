c4bbfb29aab599fdb44e16dfca9654329ff6f9e6
Aaron Wang <aaron.wang@duolingo.com>
2025-04-10 15:44:20 -0700

    Add functionalities

--- src/mcp_jenkins/server/build.py
+++ src/mcp_jenkins/server/build.py
@@ -15,7 +15,7 @@ async def get_running_builds(ctx: Context) -> list[dict]:
 
 
 @mcp.tool()
-async def get_build_info(ctx: Context, fullname: str, build_number: int | None = None) -> dict:
+async def get_build_info(ctx: Context, fullname: str, build_number: str) -> dict:
     """
     Get specific build info from Jenkins
 
@@ -26,8 +26,7 @@ async def get_build_info(ctx: Context, fullname: str, build_number: int | None =
     Returns:
         Build: The build info
     """
-    if build_number is None:
-        build_number = client(ctx).job.get_job_info(fullname).lastBuild.number
+    build_number = int(build_number)
     return client(ctx).build.get_build_info(fullname, build_number).model_dump(exclude_none=True)
 
 
@@ -44,3 +43,19 @@ async def build_job(ctx: Context, fullname: str, parameters: dict = None) -> int
         The build number of the job
     """
     return client(ctx).build.build_job(fullname, parameters)
+
+
+@mcp.tool()
+async def get_build_logs(ctx: Context, fullname: str, build_number: str) -> str:
+    """
+    Get logs from a specific build in Jenkins
+
+    Args:
+        fullname: The fullname of the job
+        build_number: The number of the build
+
+    Returns:
+        str: The logs of the build
+    """
+    build_number = int(build_number)
+    return client(ctx).build.get_build_logs(fullname, build_number)
